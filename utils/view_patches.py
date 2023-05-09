37189474460593a0a0458d9fadef8e2cee0c84d7
crohlicek <chris@rohlicek.com>
2023-05-09 16:13:15 -0400

    CLI param fix

--- utils/view_patches.py
+++ utils/view_patches.py
@@ -21,9 +21,9 @@ def view_patches(h5_file, num_patches, output_dir, shuffle=False):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Helper script for quick visualization of patches from h5 files')
-    parser.add_argument('--input_h5', help='input .h5 file containing patches to be visualized')
-    parser.add_argument('--num_patches', help='number of patches to be viewed from dataset')
-    parser.add_argument('--output_dir', help='output image directory')
+    parser.add_argument('--input_h5', type=str, help='input .h5 file containing patches to be visualized')
+    parser.add_argument('--num_patches', type=int, help='number of patches to be viewed from dataset')
+    parser.add_argument('--output_dir', type=str, help='output image directory')
     parser.add_argument('--shuffle', action='store_true', help='bool flag for drawing random patches from the file')
     args = parser.parse_args()
-    view_patches(args.input_h5, args.num_patches)
+    view_patches(args.input_h5, args.num_patches, args.output_dir, shuffle=args.shuffle)
